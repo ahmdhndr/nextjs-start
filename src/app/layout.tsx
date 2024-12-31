741e4a90fe6932e32e9b5d39a931987851759198
Achmad Hendarsyah <ahmdhndrsyh@gmail.com>
2024-12-31 21:35:03 +0700

    base not found page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import { cn } from "@/lib/utils";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "A basic starter for Next.js with Shadcn UI & Dark Mode",
+  title: "Basic Starter Next.js, Shadcn UI",
 };
 
 const jakartaSans = Plus_Jakarta_Sans({
@@ -21,19 +21,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📦</text></svg>"
-        />
+        <link rel="icon" type="image/svg+xml" href="/logo.svg" />
       </head>
       <body
         className={cn("h-full w-full overflow-hidden", jakartaSans.className)}
       >
         <ThemeProvider>
           <Navbar />
-          <main className="flex h-full w-full flex-col">{children}</main>
+          <main className="flex h-full w-full flex-col p-4">{children}</main>
         </ThemeProvider>
       </body>
     </html>
